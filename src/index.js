f7b045ab80b6dc4e8a16576743bb93cce1b034a2
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-09 16:03:22 +0200

    js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-// Напишіть функцію `groupBy()`, яка групує елементи з масиву за значенням, що повертається зворотним викликом, коли елемент із масиву передається як аргумент.
-```js
-const users = [
-  {
-    name: 'John',
-    yearOfBirth: 1988,
-    placeOfBirth: 'New York',
-  },
-  {
-    name: 'Nancy',
-    yearOfBirth: 1963,
-    placeOfBirth: 'New York',
-  },
-  {
-    name: 'John',
-    yearOfBirth: 1980,
-    placeOfBirth: 'Toronto',
-  },
-];
+// const { default: axios } = require("axios");
 
-function groupBy(users, callback){}
+// Напишіть функцію `groupBy()`, яка групує елементи з масиву за значенням, що повертається зворотним викликом, коли елемент із масиву передається як аргумент.js
+// const users = [
+//   {
+//     name: 'John',
+//     yearOfBirth: 1988,
+//     placeOfBirth: 'New York',
+//   },
+//   {
+//     name: 'Nancy',
+//     yearOfBirth: 1963,
+//     placeOfBirth: 'New York',
+//   },
+//   {
+//     name: 'John',
+//     yearOfBirth: 1980,
+//     placeOfBirth: 'Toronto',
+//   },
+// ];
 
-console.log(groupBy(users, objectEl => objectEl.placeOfBirth));
+// function groupBy(users, callback){}
+
+// console.log(groupBy(users, objectEl => objectEl.placeOfBirth));
 //Очікуваний резульат:
 // {
 //   ❗New York: [
@@ -43,4 +44,33 @@ console.log(groupBy(users, objectEl => objectEl.placeOfBirth));
 //     }
 //   ]
 // }
-```
+
+import axios from "axios";
+
+// GET / employees;
+// GET / employees / { id };
+// POST / employees;
+// PUT / employees / { id };
+// PATCH / employees / { id };
+// DELETE / employees / { id };
+
+
+const BASE_URL = 'http://localhost:4000/';
+
+
+function getEmployees() {
+  return fetch(`${BASE_URL}/employees`).then(res => res.json);
+
+}
+getEmployees().then(console.log)
+
+function getEmployeesA() {
+ return axios.get(`${BASE_URL}/employees`).then(res => res.data);
+
+}
+getEmployeesA();
+
+
+function getEmployeesById(id) {
+  return fetch(`${BASE_URL}/employees/${id}.then(res => res.json())`);
+}
